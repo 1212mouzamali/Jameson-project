b7455b85958e1ba52c14e6ae9f9ade01cb1c6859
1212mouzamali <mouzamali47@gmail.com>
2024-06-14 18:06:37 +0500

    Initial commit

---
+++ js/jameson.js
@@ -0,0 +1,12 @@
+function hello(){
+    document.querySelector(".nav-third").style.transform="translateY(600px)";
+    document.querySelector(".nav-third").style.transition="5s";
+}
+
+function cross(){
+    document.querySelector(".nav-third").style.transform="translateY(-300px)";
+    document.querySelector(".nav-third").style.transition="5s";
+}
+
+
+
